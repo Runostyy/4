92c974956925d4118e57725cff075e6ed5dee9bb
Runos <Runos@RUNOSTY>
2024-12-12 23:09:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 10; // Приклад значення n
+        var sum = Enumerable.Range(2, n - 1)  // Генеруємо числа від 2 до n
+                            .Where(x => x % 2 == 0)  // Фільтруємо парні числа
+                            .Sum();  // Обчислюємо суму парних чисел
+
+        Console.WriteLine("Сума парних чисел від 2 до " + n + " = " + sum);
+    }
+}
